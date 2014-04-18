7bd32bd00925feaa3872916bc5f3cd3d67586229
Janek Paw <explicite@windowslive.com>
2014-04-18 20:45:05 +0200

    new test functions: Ackley's and Beales

--- src/test.c
+++ src/test.c
@@ -6,36 +6,88 @@
 #include <math.h>
 
 double mc_cornic(double*);
+double ackleys(double*);
+double beales(double*);
 
 int main()
 {
+	unsigned int wolfs 		= 300;
+	unsigned int iterations = 1000;
 	Fun fun;
 
 	fun.size = 2;
 
-	double* bounds = (double*) malloc(4*sizeof(double));
+	double* bounds 	= (double*) malloc(4*sizeof(double));
+	double* min_pos = (double*) malloc(2*sizeof(double));
+
+	//Mc Cornic
 	bounds[0] = -1.5;
 	bounds[1] = 4;
-	bounds[2] = -3.0;
-	bounds[3] = 4.0;
-	fun.bounds = bounds;
+	bounds[2] = -3;
+	bounds[3] = 4;
 
-	fun.val = &mc_cornic;
+	fun.bounds 	= bounds;
+	fun.val 	= &mc_cornic;
 
-	double* min_pos = (double*) malloc(2*sizeof(double));
 	min_pos[0] = -0.54719;
 	min_pos[1] = -1.54719;
 
-	double min_val = mc_cornic(min_pos);
+	double min_mc_cornic = mc_cornic(min_pos);
+	double cmin_mc_cornic = mc_cornic(min(&fun, wolfs, iterations));
+	printf("Mc Cornic min: %f, computed: %f\n", min_mc_cornic, cmin_mc_cornic);
 
-	double computed_min_val = mc_cornic(min(&fun, 10000, 1000));
+	//Ackley's
+	bounds[0] = -5;
+	bounds[1] = 5;
+	bounds[2] = -5;
+	bounds[3] = 5;
 
-	printf("%f, %f\n", min_val, computed_min_val);
+	fun.bounds 	= bounds;
+	fun.val 	= &ackleys;
+
+	min_pos[0] = 0;
+	min_pos[1] = 0;
+
+	double min_ackleys = ackleys(min_pos);
+	double cmin_ackleys = ackleys(min(&fun, wolfs, iterations));
+	printf("Ackleys min: %f, computed: %f\n", min_ackleys, cmin_ackleys);
+
+	//Beales
+	bounds[0] = -4.5;
+	bounds[1] = 4.5;
+	bounds[2] = -4.5;
+	bounds[3] = 4.5;
+
+	fun.bounds 	= bounds;
+	fun.val 	= &beales;
+
+	min_pos[0] = 3;
+	min_pos[1] = 0.5;
+
+	double min_beales = beales(min_pos);
+	double cmin_beales = beales(min(&fun, wolfs, iterations));
+	printf("Beales min: %f, computed: %f\n", min_beales, cmin_beales);
+
 	return 0;
 }
 
-double mc_cornic(double* xs) {
+double mc_cornic(double* xs) 
+{
 	double x = xs[0];
 	double y = xs[1];
 	return (sin(x + y) + ((x - y) * (x - y)) - (1.5 * x) + (2.5 * y) + 1.0);
+}
+
+double ackleys(double* xs) 
+{
+	double x = xs[0];
+	double y = xs[1];
+	return(-20 * exp(-0.2 * sqrt(0.5 * ((x * x) + (y * y)))) - exp(0.5 * (cos(2.0 * M_PI * x) + cos(2.0 * M_PI * y))) + 20.0 + M_E);
+}
+
+double beales(double* xs)
+{
+	double x = xs[0];
+	double y = xs[1];
+	return(((1.5 - x + (x * y)) * (1.5 - x + (x * y))) + ((2.25 - x + (x * y * y)) * (2.25 - x + (x * y * y))) + ((2.625 - x + (x * y * y * y)) * (2.625 - x + (x * y * y * y))));
 }
